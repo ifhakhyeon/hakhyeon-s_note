c7d4c45c6dfb691dd55ad7db2c1dc8bfaa6d12e7
Hakheon <vacffm@naver.com>
2022-01-18 17:53:25 +0900

    1/18 17:53 싸지방

--- 파이썬/연습장.py
+++ 파이썬/연습장.py
@@ -1,7 +1,7 @@
 a = [-7, 4, -3, 6, 3, -8, 3, 4]
 
 #동적 계획법! 파이썬 감각 살리기..
-#
+#부분합의 최댓값 구하기
 def fastestMaxSum(vector):
     N = len(vector) ; ret = -987645321 ; psum = 0
 
@@ -18,3 +18,4 @@ def fastestMaxSum(vector):
 print(fastestMaxSum(a))
 
 #P. 119 설명 이해
+#인덱스를 반환하기 위해서는 후처리를 해야하구나..
