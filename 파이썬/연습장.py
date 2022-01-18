36c0525af154c873aeb3078bed9dc9d84971decf
Hakheon <vacffm@naver.com>
2022-01-18 17:03:02 +0900

    1/18 17:01 싸지방

--- 파이썬/연습장.py
+++ 파이썬/연습장.py
@@ -1,15 +1,20 @@
 a = [-7, 4, -3, 6, 3, -8, 3, 4]
 
 #동적 계획법! 파이썬 감각 살리기..
+#
 def fastestMaxSum(vector):
     N = len(vector) ; ret = -987645321 ; psum = 0
 
     for i in range(N):
         psum = max(psum,0) + vector[i]
-        print("psum :",psum, i)
+        # 동작을 알기위한 테스트코드
+        # print("psum :",psum, i)
         ret = max(psum, ret)
-        print("ret :",ret, i)
+        # 동작을 알기위한 테스트코드
+        # print("ret :",ret, i)
 
     return ret
 
 print(fastestMaxSum(a))
+
+#P. 119 설명 이해
