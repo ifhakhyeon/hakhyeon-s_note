565414970a700212adda15863e10a3de57891801
Hakheon <vacffm@naver.com>
2022-03-18 21:48:50 +0900

    3/18 싸지방 테스트

---
+++ 파이썬/백준/백준 1655 맥스힙 사용.py
@@ -0,0 +1,23 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+n = int(input())
+left = []
+righ = []
+for i in range(n):
+    num = int(sys.stdin.readline())
+
+    if len(left) == len(righ):
+        heapq.heappush(left, -num)
+    else:
+        heapq.heappush(righ, num)
+
+    if righ and righ[0] < -left[0]:
+        leftValue = heapq.heappop(left)
+        rightValue = heapq.heappop(righ)
+
+        heapq.heappush(left, -rightValue)
+        heapq.heappush(righ, -leftValue)
+
+    print(-left[0])
