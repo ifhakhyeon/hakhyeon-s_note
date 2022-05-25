4ed16eaf201317e3e0b280c5cb12821be144f8b5
Hakheon <vacffm@naver.com>
2022-05-25 22:14:55 +0900

    5/25 군대에서의 마지막 commit 정처기 공부 화이팅

--- 파이썬/습장3.py
+++ 파이썬/습장3.py
@@ -1,14 +1,28 @@
 import math
 import sys
-import heapq
 from math import sqrt
 input = sys.stdin.readline
 
-E = map(int, input().split())
+E = int(input())
+
 Vroot = [i for i in range(E + 1)]
 Elist = []
-for _ in range(E):
-    Elist.append(list(map(int, input().split())))
+star = [list(map(float, input().split()))]
+n = 2
+
+def cal(a, b):
+    x1, y1 = a[0], a[1]
+    x2, y2 = b[0], b[1]
+    return math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
+
+
+for _ in range(E-1):
+    k = list(map(float, input().split()))
+    for i in range(1, n):
+        Elist.append([i, n, cal(star[i-1], k)])
+    star.append(k)
+    n += 1
+
 
 Elist.sort(key=lambda x: x[2])
 
